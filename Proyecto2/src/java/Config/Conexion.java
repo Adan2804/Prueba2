a1371ad0a9e620c29b1a1df7404fbbb80a49fb9a
JhonatanSA <69599115+JhonatanSA@users.noreply.github.com>
2020-10-29 12:00:06 -0500

    ruta de conexion

--- Proyecto2/src/java/Config/Conexion.java
+++ Proyecto2/src/java/Config/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
     public Conexion(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/proyecto","root","root");            
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/proyecto","r","root");            
         } catch (Exception e) {
             System.err.println("Error"+e);
         }
